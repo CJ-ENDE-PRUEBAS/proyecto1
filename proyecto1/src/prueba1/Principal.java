74475503b496f10dcf5c0fedcb21b62336b621d5
usu2 <usu2@prueba.com>
2022-01-24 08:53:07 +0100

    Cambios en clase Principal. Rama cambio1

--- proyecto1/src/prueba1/Principal.java
+++ proyecto1/src/prueba1/Principal.java
@@ -6,6 +6,8 @@ public class Principal {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo desde java-git");
 		System.out.println("Adios");
+		System.out.println("Clase Principal");
+		System.out.println("Pruebas Git. Clase Principal");
 
 	}
 
