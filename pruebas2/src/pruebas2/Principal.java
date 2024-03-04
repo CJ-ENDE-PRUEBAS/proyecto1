7eca1cfbdcf95cbcb920ac9a14e9a6757154f872
CJ-ENDE-PRUEBAS <ende.ciudadjardin@gmail.com>
2024-03-04 19:54:31 +0100

    Tercera validación

--- pruebas2/src/pruebas2/Principal.java
+++ pruebas2/src/pruebas2/Principal.java
@@ -5,8 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo java-git");
 		System.out.println("Hola mundo java-git second");
-		
-
+		System.out.println("Hola mundo java-git third");
 	}
 
 }
