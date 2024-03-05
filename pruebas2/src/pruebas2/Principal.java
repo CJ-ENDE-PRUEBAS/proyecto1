1039ff9ab3d0f342d46ae13835170f7faa1edfb7
CJ-ENDE-PRUEBAS <ende.ciudadjardin@gmail.com>
2024-03-05 08:45:41 +0100

    Cambio 1 en clase Principal

--- pruebas2/src/pruebas2/Principal.java
+++ pruebas2/src/pruebas2/Principal.java
@@ -4,9 +4,9 @@ public class Principal {
 	
 	public static void main(String[] args) {
 		System.out.println("Hola mundo java-git");
-		System.out.println("Hola mundo java-git second");
-		System.out.println("Hola mundo java-git third");
-		System.out.println("Hola mundo java-git fourth");
+		System.out.println("Hola mundo java-git");
+		System.out.println("Adios");
+		System.out.println("Pruebas Git. Se añade Cambio1");
 	}
 
 }
